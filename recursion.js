442a2f3ec3e873c0e4388084101d1129c4d52c6c
Rei Nelku <85695388+ReiNelku@users.noreply.github.com>
2025-07-10 10:56:51 +0200

    feat: Calculate Fibonacci recursively

--- recursion.js
+++ recursion.js
@@ -23,3 +23,15 @@ function fibs(n) {
 
   return answer;
 }
+
+function fibsRec(n) {
+  if (n === 1) {
+    return 0;
+  } else if (n === 2) {
+    return 1;
+  }
+
+  return fibsRec(n - 1) + fibsRec(n - 2);
+}
+
+console.log(fibsRec(1));

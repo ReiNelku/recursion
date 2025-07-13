572a7f6eb01008b99a2075f7c003133221af4991
Rei Nelku <85695388+ReiNelku@users.noreply.github.com>
2025-07-13 22:21:16 +0200

    feat: Implement MergeSort

--- recursion.js
+++ recursion.js
@@ -34,4 +34,47 @@ function fibsRec(n) {
   return fibsRec(n - 1) + fibsRec(n - 2);
 }
 
-console.log(fibsRec(1));
+function mergeSort(array, start, end) {
+  if (start < end) {
+    const mid = Math.floor((end + start) / 2);
+    mergeSort(array, start, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, start, mid, end);
+  }
+}
+
+function merge(array, start, mid, end) {
+  const tempArray = [];
+
+  let i = start;
+  let j = mid + 1;
+  let k = 0;
+
+  while (i <= mid && j <= end) {
+    if (array[i] <= array[j]) {
+      tempArray.push(array[i]);
+      i++;
+      k++;
+    } else {
+      tempArray.push(array[j]);
+      j++;
+      k++;
+    }
+  }
+
+  while (i <= mid) {
+    tempArray[k] = array[i];
+    i++;
+    k++;
+  }
+
+  while (j <= end) {
+    tempArray[k] = array[j];
+    j++;
+    k++;
+  }
+
+  for (let i = start; i <= end; i++) {
+    array[i] = tempArray[i - start];
+  }
+}
